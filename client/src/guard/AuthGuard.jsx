6c0e38579bea3b38de355e27a0f1be759067353f
ariquelmesanchez <ariquelme.snchz@gmail.com>
2025-02-21 02:04:15 -0300

    arreglado authguard y proffile

--- client/src/guard/AuthGuard.jsx
+++ client/src/guard/AuthGuard.jsx
@@ -1,23 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
-import UserProvider from '../store/UserContext';
-
+import { UserContext } from '../store/UserContext'
 const AuthGuard = ({ redirectTo = '/', requiresAuth, children }) => {
-  const { token } = useContext(UserProvider)
+  const { token } = useContext(UserContext)
 
   // Redirect unauthenticated users to the login page if authentication is required
   if (requiresAuth && !token) {
-    return <Navigate to='/login' replace />;
+    return <Navigate to='/login' replace />
   }
 
   // Redirect authenticated users away from login and registration pages
   if (!requiresAuth && token) {
     if (window.location.pathname === '/login' || window.location.pathname === '/register') {
-      return <Navigate to='/' replace />;
+      return <Navigate to='/' replace />
     }
   }
 
-  return children || <Outlet />;
-};
+  return children || <Outlet />
+}
 
-export default AuthGuard;
+export default AuthGuard
