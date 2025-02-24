f03dd26fc8fb49e40215d4ea378881dae1a51b62
ariquelmesanchez <ariquelme.snchz@gmail.com>
2025-02-24 02:40:46 -0300

    corregidos errores de comas y demas en constants.js

--- client/src/config/constans.js
+++ client/src/config/constans.js
@@ -1,7 +1,6 @@
-
-const urlBase = import.meta.env.VITE_URLBASE;
+const urlBase = import.meta.env.VITE_URLBASE
 
 export const ENDPOINT = {
   login: `${urlBase}/login`,
   users: `${urlBase}/register`
-};
+}
