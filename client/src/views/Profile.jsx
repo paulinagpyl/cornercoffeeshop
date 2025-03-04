b64c1bdac49527b0bebf16538244b1dd8145e57c
DanteVasquezRivas <147457552+DanteVasquezRivas@users.noreply.github.com>
2025-03-03 22:12:58 -0300

    Cambio en tarjeta Profile

--- client/src/views/Profile.jsx
+++ client/src/views/Profile.jsx
@@ -32,7 +32,7 @@ const Profile = () => {
     <div className='vh-100' style={{ backgroundColor: '#eee' }}>
       <MDBContainer>
         <MDBRow className='justify-content-center'>
-          <MDBCol md='9' lg='7' xl='5' className='mt-5 mb-5'>
+          <MDBCol md='9' lg='7' xl='5' className='p-4'>
             <MDBCard style={{ borderRadius: '15px', backgroundColor: '#FFFCD4' }}>
               <MDBCardBody className='p-4 text-black'>
                 {profile ? (
